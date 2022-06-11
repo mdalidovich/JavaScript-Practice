079555894692e5459271ae2136c3bc535f2d4285
Marina Dalidovich <dalidovich.marina@gmail.com>
2022-06-11 12:21:21 -0400

    Count chars in String

---
+++ count-chars-in-string.js
@@ -0,0 +1,12 @@
+function charCount(str, letter){
+    let count = 0;
+
+    for(let i = 0; i < str.length; i++){
+        if(str[i] = letter) count ++;
+    }
+
+    return count;
+
+}
+
+console.log(charCount('Hello World', 'o'));
