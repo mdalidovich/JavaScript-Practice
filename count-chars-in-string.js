d14856869aa90642dbc93779aa09db2e4aa5aaf8
Marina Dalidovich <dalidovich.marina@gmail.com>
2022-06-11 13:08:07 -0400

    add function to count number of a specific char in string

--- count-chars-in-string.js
+++ count-chars-in-string.js
@@ -1,12 +1,26 @@
-function charCount(str, letter){
+//count number of chars in string(space included)
+function charCount(str, el){
     let count = 0;
 
     for(let i = 0; i < str.length; i++){
-        if(str[i] = letter) count ++;
+        
+        if(str[i] = el) count ++;
     }
-
     return count;
-
 }
 
 console.log(charCount('Hello World', 'o'));
+
+//count number of specific chars in string
+function countLetters(str, letter){
+    let count = 0;
+    
+    for(let i = 0; i < str.length; i++){
+      if(str.charAt(i) === letter){
+        count ++
+      }
+    }
+    return count;
+  }
+  
+  console.log(countLetters('Hello World', 'o'))
