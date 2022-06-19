093126acdaa3414ace2b5bd0ac317f327c891d2e
Marina Dalidovich <dalidovich.marina@gmail.com>
2022-06-18 20:53:07 -0400

    add file with random number generator

---
+++ random-number-generator.js
@@ -0,0 +1,10 @@
+//one line solution
+Math.floor(Math.random() * 10 + 1);
+
+
+//using consts
+const step1 = Math.random();
+const step2 = step1 * 10;
+const step3 = Math.floor(step2);
+const step4 = step3 + 1;
+console.log(step4);
